9eda841c275f67b6aa118674017c5b071a0d97dc
mambax7 <mambax7@gmail.com>
2020-11-12 07:29:05 -0500

    Unnecessary double quotes

--- include/index.php
+++ include/index.php
@@ -1,4 +1,4 @@
  <?php
 header('HTTP/1.0 404 Not Found');
 
- <span style="color: blue; ">" . htmlspecialchars($_GET['mes'], ENT_QUOTES) . '</span>
+ <span style='color: blue; '>" . htmlspecialchars($_GET['mes'], ENT_QUOTES) . '</span>
